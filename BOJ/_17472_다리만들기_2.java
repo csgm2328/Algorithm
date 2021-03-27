fb94677f829633c1d269ea25681d5214e1459697
chupszi <csgm2328@gmail.com>
2021-03-27 17:25:44 +0900

    210326 BFS + 경로만들기 + 가중치 배열 생성 + Prim

--- BOJ/_17472_다리만들기_2.java
+++ BOJ/_17472_다리만들기_2.java
@@ -10,18 +10,16 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-import javax.print.attribute.standard.Sides;
-
-// 다리 놓기
+// 다리 만들기 2
 // BFS로 면적번호를 써 놓은 후 다시 BFS로 면적이 닿으면 그 섬까지의 가중치로
-// 인접리스트를 만든다
+// 인접배열을 만든다
 // 그리고 다익스트라 아니고 프림
 public class _17472_다리만들기_2 {
 	static BufferedReader br;
 	static StringTokenizer st;
 	static int N, M;
 	static int[][] arr;
-	static boolean[][] visited; //섬 번호를 2부터 쓴다면 필요 없음
+	static boolean[][] visited; // 섬 번호를 2부터 쓴다면 필요 없음
 	static int[][] adjIsland;
 
 	static int[] dr = { 0, -1, 1, 0 };
@@ -50,7 +48,7 @@ public class _17472_다리만들기_2 {
 			for (int j = 0; j < M; j++)
 				arr[i][j] = Integer.parseInt(st.nextToken());
 		}
-		//섬 번호 표시
+		// 섬 번호 표시
 		int island_No = 1;
 		Queue<xy> q = new LinkedList<xy>();
 		for (int i = 0; i < N; i++) {
@@ -65,9 +63,10 @@ public class _17472_다리만들기_2 {
 			}
 		}
 		// 섬번호 써놨으면 자신 아닌거 만나면 가중치 정보 저장
-		adjIsland = new int[island_No][island_No]; // 섬 개수만큼
+		adjIsland = new int[island_No][island_No]; // 섬 개수+1만큼
 		for (int[] x : adjIsland)
 			Arrays.fill(x, Integer.MAX_VALUE);
+		//섬 구분해서 큐에 넣고 경로 찾기
 		for (int no = 1; no < island_No; no++) {
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < M; j++) {
@@ -80,45 +79,44 @@ public class _17472_다리만들기_2 {
 			searchPath(q, no); // 경로 찾아서 가중치 배열에 저장
 		}
 //		 가중치 배열 준비되었으면 프림
-		int[] savedShortestPath = new int[island_No];
-		boolean[] adjVisited = new boolean[island_No];
-		List<Integer> S = new ArrayList<>();
+		int[] minEdge = new int[island_No];
+		boolean[] verVisited = new boolean[island_No];
+		List<Integer> S = new ArrayList<>(); //Spanning Tree
 		// 시작 정점 셋팅
-		Arrays.fill(savedShortestPath, Integer.MAX_VALUE);
-		savedShortestPath[1] = 0;
+		Arrays.fill(minEdge, Integer.MAX_VALUE);
+		minEdge[1] = 0;
 		// 1. 모든 정점이 신장트리에 포함 될 때 까지
 		int sum = 0;
 		while (S.size() < island_No - 1) {
-			// 2. 미포함 정점 중 신장트리에 포함되는 가장 최소비용의 정점 선택
-			int selectedVertex = extractMin(adjVisited, savedShortestPath);
+			// 2. 미포함 정점 중 신장트리에 가장 최소비용으로 포함가능한 정점 선택
+			int minVertex = extractMin(verVisited, minEdge);
 			// 연결끊기면 종료
-			if (savedShortestPath[selectedVertex] == Integer.MAX_VALUE) {
+			if (minEdge[minVertex] == Integer.MAX_VALUE) {
 				sum = -1;
 				break;
 			}
-			// 3. 선택된 정점의 연결된 정점들에 가중치 갱신하고
+			// 3. 선택된거 신장트리에 포함시킴
+			verVisited[minVertex] = true;
+			S.add(minVertex);
+			sum += minEdge[minVertex];
+			
+			// 4. 선택된 정점의 연결된 정점들에 가중치 갱신하고
 			for (int i = 1; i < island_No; i++) {
-				if (adjVisited[i] || adjIsland[selectedVertex][i] == Integer.MAX_VALUE) // 연결되있고 아직 미포함인지
-					continue;
-				// 더 짧게 포함할 수 있는 경로 있으면 갱신
-				else if (savedShortestPath[i] > adjIsland[selectedVertex][i])
-					savedShortestPath[i] = adjIsland[selectedVertex][i];
+				// 아직 미포함 & 더 작은 가중치로 포함 가능하면 (INF 초기화라 연결체크 안해도됨)
+				if (!verVisited[i] && minEdge[i] > adjIsland[minVertex][i]) 
+					minEdge[i] = adjIsland[minVertex][i];
 			}
-			// 4. 선택된거 신장트리에 포함시킴
-			adjVisited[selectedVertex] = true;
-			S.add(selectedVertex);
-			sum += savedShortestPath[selectedVertex];
 		}
 		System.out.print(sum);
 	}
 
-	private static int extractMin(boolean[] visited, int[] savedShortestPath) {
+	private static int extractMin(boolean[] visit, int[] minEdge) {
 		int min = Integer.MAX_VALUE;
 		int minVertex = 0;
-		for (int i = 1; i < visited.length; i++) {
+		for (int i = 1; i < visit.length; i++) {
 			// 아직 S에 포함안되었으면서(미방문) 가중치가 가장 적은거 선택
-			if (!visited[i] && savedShortestPath[i] < min) {
-				min = savedShortestPath[i];
+			if (!visit[i] && minEdge[i] < min) {
+				min = minEdge[i];
 				minVertex = i;
 			}
 		}
@@ -132,27 +130,27 @@ public class _17472_다리만들기_2 {
 				int nr = cur.r + dr[dir];
 				int nc = cur.c + dc[dir];
 				if (isInBoundary(nr, nc) && arr[nr][nc] == 0) { // 다음이 바다이면
-					stretch(nr, nc, dir, no); // 그방향으로 직진
+					straight(nr, nc, dir, no); // 그방향으로 직진해서 다리 놓기 가능하면 인접배열에 표시
 				}
 			}
 		} // end while
 		q.clear();
 	}
 
-	private static void stretch(int nr, int nc, int dir, int from) {
+	private static void straight(int nr, int nc, int dir, int from) {
 		int cnt = 1; // 현재 카운트 1
 		nr += dr[dir];
 		nc += dc[dir];
 		// 끝나거나 다른 섬나올때까지 직진
 		while (isInBoundary(nr, nc)) {
-			if (arr[nr][nc] != 0 ) { // 섬을 만나면
-				if (cnt == 1 || arr[nr][nc] == from) //반례 : 가다가 또 나의 섬을 만나면
+			if (arr[nr][nc] != 0) { // 섬을 만나면
+				if (cnt < 2 || arr[nr][nc] == from) // 반례 : 가다가 또 나의 섬을 만나면
 					break; // 다리길이 1이면 더이상 진행 못하도록
 				int to = arr[nr][nc];
 				if (adjIsland[from][to] > cnt) { // 지금까지의 cnt를 from to의 가중치로 갱신
 					adjIsland[from][to] = cnt;
 				}
-				break; //섬을 만나면 반드시 나가야함 --> 실수 포인트
+				break; // 섬을 만나면 반드시 나가야함 --> 실수 포인트
 			}
 			nr += dr[dir];
 			nc += dc[dir];
