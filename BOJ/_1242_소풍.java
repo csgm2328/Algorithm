adb66d0e7d24cab0e2cfc60c47ba520524468bb9
chupszi <csgm2328@gmail.com>
2021-04-06 01:18:40 +0900

    210405 josephus

--- BOJ/_1242_소풍.java
+++ BOJ/_1242_소풍.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class _1242_소풍 {
 	static BufferedReader br;
 	static StringTokenizer st;
-	static int N,K,M;
+	static int N, K, M;
 
 	public static void main(String[] args) throws IOException {
 		// global init
@@ -17,33 +17,13 @@ public class _1242_소풍 {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken()); // 사람 수 <= 5백만
 		K = Integer.parseInt(st.nextToken()); // 몇번째 제거
-		M = Integer.parseInt(st.nextToken()); //동호가 처음에 앉은 곳
-		 
-		//상대 위치 방법
-		int cnt = 0;
-		int remove = K; //지울 위치
-		if(K%N != 0) //K가 N보다 클 경우
-			remove = K%N;
-		else //K가 0이랑 같을 경우
-			remove = N;
-		while(true) {
-			cnt++;
-			if(remove== M)
-				break;
-			if(M > remove)
-				M -= remove; //시작위치에서 동호의 상대적인 위치
-			else {
-				M -= remove;
-				M += N;
-			}
-			N--;
-			if(K%N !=0 )
-				remove = K%N;
-			else 
-				remove = N;
-		}
+		M = Integer.parseInt(st.nextToken()); // 동호가 처음에 앉은 곳
+
+		// 최적화
+		// 실제로 큐로 동작되는 걸 생각해야함 (k가 아닌 remove로 연산)
+		System.out.println(josephus(N, K, M));
 		
-		//절대 위치 방법
+		// 절대 위치 방법 - 실패
 //		int start = 1;
 //		while(N != 1) {
 //			System.out.print("시작" + start);
@@ -64,6 +44,40 @@ public class _1242_소풍 {
 //			System.out.println();
 //		}
 //		cnt++; //break로 나오든 하나남아서 나오든 ++
-		System.out.println(cnt);
+		
+	}
+
+	private static int josephus(int n, int k, int m) {
+		int remove = 0;
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) { // n개 요소 다 나가면 끝 & cnt 용도
+			int qsize = n - i + 1; // 남은 요소들의 수
+			remove = k % qsize; // 삭제할 위치는 항상 사이즈에서 k번째 꺼다
+								// m의 위치가 달라지므로 고정해도 된다
+			if (remove == 0)
+				remove = qsize;
+			if (remove == m) { // 삭제위치 == 목표 위치
+				cnt = i; 
+				break;
+			}
+			m -= remove; //타겟위치변경
+			if (m < 0)
+				m += qsize;
+		}
+		return cnt;
+	}
+
+	private static int josephus3(int n, int k, int m) {
+		int index = 0;
+		// 11025_요세푸스 3: 맨 마지막으로 나가는 요소 찾기
+		// 요소 수만큼 돌면서 위치를 K만큼 돌림
+		// 0이 기준이라 따로 if문 필요없음
+		// 하지만, 중간 요소수를 안맞춰줘서 맨 마지막 것만 맞음
+		System.out.println("지우는 순서: ");
+		for (int i = 1; i <= n; i++) { // 마지막 1개 ~ N개
+			index = (index + K) % i;
+			System.out.print(index + " ");
+		}
+		return index + 1; // 0부터 시작했으니까
 	}
 }
